d059bdb7053c61a3e7f954afaefcf3e0cf054a03
Siarhei Lunski <Siarhei.Lunski@itechart-group.com>
2019-02-15 13:56:44 +0300

    toggle menu

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 import './styles/main.scss';
+
+const MOBILE_SCREEN_SIZE = 575;
+
+window.onresize = e => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > MOBILE_SCREEN_SIZE) {
+    menuContainer.style.display = 'block';
+  } else {
+    menuContainer.style.display = 'none';
+  }
+};
+
+const menuButton = document.getElementById('menu-button');
+const menuContainer = document.getElementById('menu-container');
+
+const toggleMenu = () => {
+  const screenWidth = window.innerWidth;
+  menuContainer.style.display =
+    menuContainer.style.display === 'block' && screenWidth <= MOBILE_SCREEN_SIZE
+      ? 'none'
+      : 'block';
+};
+
+menuButton.addEventListener('click', toggleMenu);
