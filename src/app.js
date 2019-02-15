b70d72a8b6155fb43013323a5b0faf754ab4da0f
Siarhei Lunski <Siarhei.Lunski@itechart-group.com>
2019-02-15 15:36:04 +0300

    clear form inputs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const MOBILE_SCREEN_SIZE = 575;
 const menuButton = document.getElementById('menu-button');
 const menuContainer = document.getElementById('menu-container');
 const searchForm = document.getElementById('search-form');
+const clearButton = document.getElementById('button-clear');
+const searchButton = document.getElementById('button-search');
 
 const formSections = {
   flights: document.getElementById('flights-section'),
@@ -40,6 +42,7 @@ const displaySection = id => {
 };
 
 const selectMenuTab = e => {
+  clearInputFields();
   const selectedTab = e.target;
   const menuTabs = [...e.target.parentElement.children];
   displaySection(selectedTab.id);
@@ -52,5 +55,19 @@ const selectMenuTab = e => {
   });
 };
 
+const clearInputFields = () => {
+  const elements = [...searchForm.elements];
+  elements.forEach(element => {
+    element.value = '';
+  });
+};
+
+const submitSearchForm = e => {
+  const values = searchForm.elements;
+  console.log(values);
+};
+
 menuButton.addEventListener('click', toggleMenu);
 menuContainer.addEventListener('click', selectMenuTab);
+clearButton.addEventListener('click', clearInputFields);
+searchButton.addEventListener('click', submitSearchForm);
